923d7a053453b190c96baf5911d734f1e06db78e
SHASHI SHANKAR <sk7698323@gmail.com>
2024-06-28 08:40:59 +0530

    authorization completed

--- routes/user.js
+++ routes/user.js
@@ -3,20 +3,27 @@ const router = express.Router();
 const User = require("../models/user.js");
 const wrapAsync = require("../utils/wrapAsync.js");
 const passport = require("passport");
+const { saveRedirectUrl } = require("../middleware.js");
 
-router.get("/singup", (req,res) => {
+router.get("/signup", (req,res) => {
     res.render("users/signup.ejs");
 });
 
 router.post("/signup", wrapAsync(async (req,res) => {
     try {
         let {username, email, password} = req.body;
-    const newUser = new User({email,username});
+        const newUser = new User({email,username});
 
-    const registeredUser = await User.register(newUser,password);
-    console.log(registeredUser);
-    req.flash("success", "Welcome to RentEase");
-    res.redirect("/listings");
+        const registeredUser = await User.register(newUser,password);
+        console.log(registeredUser);
+        req.login(registeredUser, (err) => {
+        if(err) {
+            return next(err);
+        }
+        req.flash("success", "Welcome to RentEase");
+        res.redirect("/listings");
+    });
+    
     } catch (err) {
         req.flash("error", err.message);
         res.redirect("/singup");
@@ -27,14 +34,13 @@ router.get("/login", (req,res) =>{
     res.render("users/signin.ejs");
 });
 
-router.post("/login", passport.authenticate("local", {failureRedirect: "/login", failureFlash: true}) ,async (req,res) => {
-    try {
+router.post("/login", 
+    saveRedirectUrl,
+    passport.authenticate("local", {failureRedirect: "/login", failureFlash: true}) ,async (req,res) => {
         req.flash("success","welcome back to RentEase! You are logged in now!");
-        res.redirect("/listings");
-    } catch (err) {
-        req.flash("error", err.message);
-        res.redirect("/login");
-    }
+        let redirectUrl = res.locals.redirectUrl || "/listings";
+        res.redirect(redirectUrl);
+    
 });
 
 router.get("/logout", (req,res,next) => {
